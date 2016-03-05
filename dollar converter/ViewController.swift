81d6d4af77ff738633d4335cc9a2a71df7a165a2
John Nilsen <nilsen340@gmail.com>
2016-03-05 16:21:53 -0500

    Initial Commit

---
+++ dollar converter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dollar converter
+//
+//  Created by John Rikard Nilsen on 5/3/16.
+//  Copyright © 2016 John Rikard Nilsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ dollar converterTests/dollar_converterTests.swift
@@ -0,0 +1,36 @@
+//
+//  dollar_converterTests.swift
+//  dollar converterTests
+//
+//  Created by John Rikard Nilsen on 5/3/16.
+//  Copyright © 2016 John Rikard Nilsen. All rights reserved.
+//
+
+import XCTest
+@testable import dollar_converter
+
+class dollar_converterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
